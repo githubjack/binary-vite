ff846f50ad8d05f104fbde27377f71205c064834
githubjack <jack.ui.maynard@gmail.com>
2023-03-27 15:05:59 +0100

    1 file modified

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -3,13 +3,14 @@ import styled from 'styled-components';
 
 const Logo = styled.img`
   width: 60px;
+  height: 60px;
+  display: flex;
 `;
 
 export function Menu() {
   return (
     <Inline stretch={1} gutter='size3'>
-
-      <span>Binary logo</span>
+      <Logo src='/Binary_logo_white.png' alt='Go' />
       <Inline gutter='size3' justify='start'>
         <a href="/#">Home</a>
         <a href="/#">Profile</a>
